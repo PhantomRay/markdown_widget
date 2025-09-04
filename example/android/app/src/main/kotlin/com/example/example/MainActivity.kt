1d5c2145d9fc1de46fb9b1ffb2cfe2d6f8f5bc76
HuangRed <huangred@163.com>
2025-09-04 18:15:13 +0800

    重构example

--- example/android/app/src/main/kotlin/com/example/example/MainActivity.kt
+++ example/android/app/src/main/kotlin/com/example/example/MainActivity.kt
@@ -1,6 +1,5 @@
-package com.example.example
+package com.leo.example
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity() {
-}
+class MainActivity : FlutterActivity()
